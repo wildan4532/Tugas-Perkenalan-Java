d33dcd7bb76d86cfa9ac2f448dfced4f2c09f7a5
wildan4532 <wildan_prasetyo_33rpl@student.smktelkom-mlg.sch.id>
2024-08-01 06:14:23 +0700

    Add files via upload

---
+++ java learning/list.java
@@ -0,0 +1,13 @@
+public class list {
+    public static void main(String[] args) {
+        String judul = "Tugas Perkenalan by Bu Pashatania"; 
+        String nama = "Nama : Wildan Ahyan Daffa Rifqy Prasetyo";
+        String alamat = "Alamat : Tawang - Wates, Kab. Kediri";
+        String smp = "Asal SMP : SMPN 1 WATES";
+        String nohp = "No. hp : 082334926625";
+
+        System.out.println(judul + System.lineSeparator() + System.lineSeparator() +nama + System.lineSeparator() + alamat + System.lineSeparator() + smp + System.lineSeparator()+nohp);
+    }
+    
+}
+ 
